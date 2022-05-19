d2e92f228cc8077c0add18ab24a9158c09ebfb7c
AzizHamed <Abd.Elaziz.Hamed@e.braude.ac.il>
2022-05-19 21:33:48 +0300

    # WARNING: head commit change Merge branch 'master' of https://github.com/WardZid/Assignment3

--- G18_ZL_Client/src/boundary/fxmlControllers/CustomerSupportController.java
+++ G18_ZL_Client/src/boundary/fxmlControllers/CustomerSupportController.java
@@ -2,6 +2,7 @@ package boundary.fxmlControllers;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import control.ClientController;
@@ -95,10 +96,10 @@ public class CustomerSupportController implements Initializable {
 
     @FXML
     private VBox vb3;
-    private int selectedComplaint;
-    public static ArrayList<Integer> ComplaintsIdlist = new ArrayList<>();
-    public static ArrayList<Complaint> ComplaintsList = new ArrayList<>();
-	private String ComplaintIdString,refundString;
+    private int selectedComplaintId;
+    private Complaint selectedComplaint;
+    public static HashMap<Integer,Complaint> ComplaintMap = new HashMap<>();
+	private int isSendedReply = 0;
     public void onSelectComplaint(ActionEvent event) {
     	    }
     
@@ -107,46 +108,51 @@ public class CustomerSupportController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		//send to server
-		ComplaintsIdlist.add(1);
-		ComplaintsIdlist.add(2);
-		ComplaintsIdlist.add(3);
-		ComplaintsList.add(new Complaint(1,"123456","OnTreatment","1/5/2022",40,"Not good",null));
-		ComplaintsList.add(new Complaint(2,"543216","OnTreatment","2/5/2022",50,"Not good flower",null));
-		ComplaintsList.add(new Complaint(1,"645321","OnTreatment","3/5/2022",60,"Not good workers",null));
-
-
-		ComplaintL.getItems().addAll(ComplaintsIdlist);
+		ComplaintMap.put(1, new Complaint(1,"123456","OnTreatment","1/5/2022",40,"Not good",null));
+		ComplaintMap.put(2, new Complaint(2,"543216","OnTreatment","2/5/2022",50,"Not good flower",null));
+		ComplaintMap.put(3, new Complaint(3,"645321","OnTreatment","3/5/2022",60,"Not good workers",null));
+		ComplaintL.getItems().addAll(ComplaintMap.keySet());
+		CustomerIdT.setDisable(true);
+		ComplaintIdT.setDisable(true);
+		statusT.setDisable(true);
+		dateT.setDisable(true);
+		refundT.setDisable(true);
+		descriptionT.setDisable(true);
 		sendReplyButton.setDisable(true);
 		ComplaintL.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
 		
 			@Override
 			public void changed(ObservableValue<? extends Integer> arg0, Integer arg1, Integer arg2) {
-				selectedComplaint = ComplaintL.getSelectionModel().getSelectedItem();
-				ComplaintIdString = Integer.toString(selectedComplaint-1);
-				CustomerIdT.setText(ComplaintsList.get(selectedComplaint-1).getIdCustomer());
-				ComplaintIdT.setText(ComplaintIdString);
-				statusT.setText(ComplaintsList.get(selectedComplaint-1).getStatus());
-				dateT.setText(ComplaintsList.get(selectedComplaint-1).getDate());
-				refundString = Double.toString(ComplaintsList.get(selectedComplaint-1).getRefund());
-				refundT.setText(refundString);
-				descriptionT.setText(ComplaintsList.get(selectedComplaint-1).getComplaint());
+				if(isSendedReply == 0) {
+				selectedComplaintId = ComplaintL.getSelectionModel().getSelectedItem();
+				selectedComplaint = ComplaintMap.get(selectedComplaintId);
+				ComplaintIdT.setText(Integer.toString(selectedComplaintId));
+				CustomerIdT.setText(selectedComplaint.getIdCustomer());
+				statusT.setText(selectedComplaint.getStatus());
+				dateT.setText(selectedComplaint.getDate());
+				refundT.setText(Double.toString(selectedComplaint.getRefund()));
+				descriptionT.setText(selectedComplaint.getComplaint());
+				//descriptionT.setDisable(true);
 				replyT.setText("");
 				sendReplyButton.setDisable(false);
+				}
+				isSendedReply = 0;
 			}
 			
 		});
 	}
 	public void onSendReply(ActionEvent event) {
-		System.out.println(selectedComplaint);
-		ComplaintsList.remove(selectedComplaint-1);
-		ComplaintsIdlist.remove(selectedComplaint -1);
+		//sendToServer
+		isSendedReply = 1;
+		ComplaintMap.remove(selectedComplaintId);
 		ComplaintL.getItems().clear();
-		ComplaintL.getItems().addAll(ComplaintsIdlist);
+		ComplaintL.getItems().addAll(ComplaintMap.keySet());
 		CustomerIdT.setText("");
 		ComplaintIdT.setText("");
 		statusT.setText("");
 		dateT.setText("");
 		refundT.setText("");
+		descriptionT.setText("");
 		replyT.setText("");
 		sendReplyButton.setDisable(true);
 	}
