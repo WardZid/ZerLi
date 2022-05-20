d95dc49b59d5f2b3a3cb8439951c5061b29642e2
WardZid <86842722+WardZid@users.noreply.github.com>
2022-05-20 17:05:40 +0300

    s

--- G18_ZL_Client/src/boundary/fxmlControllers/LogInController.java
+++ G18_ZL_Client/src/boundary/fxmlControllers/LogInController.java
@@ -3,10 +3,15 @@ package boundary.fxmlControllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import boundary.ClientView;
+import control.ClientController;
+import control.MainController;
+import entity.MyMessage.MessageType;
 import entity.User;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -32,20 +37,32 @@ public class LogInController implements Initializable {
     
     @FXML
     private TextField usernameTF;
+    
+    @FXML
+    private Label errorLbl;
 
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		System.out.println("hello");
 		welcomeIV.setImage(new Image("boundary/media/zli-welcome.png"));
 		userIconIv.setImage(new Image("boundary/media/user-icon.png"));
 		passIconIV.setImage(new Image("boundary/media/pass-icon.png"));
 
-		System.out.println("hi");
 	}
 	
 	public void onLogInPressed() {
+		if(usernameTF.getText().isEmpty() || passwordPF.getText().isEmpty()) {
+			errorLbl.setText("*Please fill the missing fields");
+			return;
+		}
+		MainController.getMyClient().send(MessageType.GET, "/user", new User(usernameTF.getText(),passwordPF.getText()));
 		
+		if(ClientConsoleController.getUser()==null) {
+			errorLbl.setText("*Incorrect user or pass.");
+			return;
+		}
+		
+		ClientView.setUpClientConsole();
 	}
 
 }
