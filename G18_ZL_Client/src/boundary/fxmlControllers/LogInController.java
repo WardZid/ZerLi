f263d54c44f629969ff2b562247046b53a800fae
WardZid <86842722+WardZid@users.noreply.github.com>
2022-05-20 18:07:19 +0300

    import clean

--- G18_ZL_Client/src/boundary/fxmlControllers/LogInController.java
+++ G18_ZL_Client/src/boundary/fxmlControllers/LogInController.java
@@ -4,13 +4,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import boundary.ClientView;
-import control.ClientController;
 import control.MainController;
 import entity.MyMessage.MessageType;
 import entity.User;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
