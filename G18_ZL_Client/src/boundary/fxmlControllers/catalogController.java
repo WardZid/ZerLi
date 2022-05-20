1d594998baa2ca36bd0f488b32f20cba543859ea
amer635a <72458035+amer635a@users.noreply.github.com>
2022-05-20 17:02:51 +0300

    new catalog view

---
+++ G18_ZL_Client/src/boundary/fxmlControllers/catalogController.java
@@ -0,0 +1,19 @@
+package boundary.fxmlControllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class catalogController  implements Initializable { 
+
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
