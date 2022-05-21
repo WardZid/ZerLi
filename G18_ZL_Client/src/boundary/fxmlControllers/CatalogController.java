518aedc98a63df4b51003c87b199f56e8b12f999
WardZid <86842722+WardZid@users.noreply.github.com>
2022-05-21 17:41:14 +0300

    catalog fix

--- G18_ZL_Client/src/boundary/fxmlControllers/CatalogController.java
+++ G18_ZL_Client/src/boundary/fxmlControllers/CatalogController.java
@@ -1,6 +1,5 @@
 package boundary.fxmlControllers;
 
-import java.awt.ScrollPane;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -15,19 +14,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
 public class CatalogController implements Initializable {
 
 	@FXML
-	private static GridPane grid;
-
-	@FXML
-	private ScrollPane scroll;
+	private GridPane grid;
 
 	private static ArrayList<Item> items;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		int column = 0;
@@ -41,74 +37,26 @@ public class CatalogController implements Initializable {
 			for (int i = 0; i < items.size(); i++) {
 
 				FXMLLoader fXMLLoader = new FXMLLoader(ClientView.class.getResource("fxmls/catalog-item-view.fxml"));
-				//fXMLLoader.setLocation();
-
-				CatalogItemController catalogItemController = fXMLLoader.getController();
-                  
-				catalogItemController.setData(items.get(i));
-
-				AnchorPane anchorPane = fXMLLoader.load();
-				grid.add(anchorPane, column++, row);
-
-				if (column == 3) {
-					column = 0;
-					row++;
-				}
-
-				GridPane.setMargin(anchorPane, new Insets(10));
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
-
-	public static void setItems(ArrayList<Item> content) {
-		int column = 0;
-		int row = 0;
-		items = content;
-		Node currentNode;
-		System.out.println(items.toString());
-		
-		
-		/*
-		try {
-			currentNode = FXMLLoader.load(ClientView.class.getResource("fxmls/catalog-item-view.fxml"));
-			grid.getChildren().clear();
-			grid.getChildren().add(currentNode);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
-		
-		
-
-		 
-		try {
-			for (int i = 0; i < items.size(); i++) {
-
-				FXMLLoader fXMLLoader = new FXMLLoader(ClientView.class.getResource("fxmls/catalog-item-view.fxml"));
-				//fXMLLoader.setLocation(ClientView.class.getResource("fxmls/catalog-item-view.fxml"));
-				AnchorPane anchorPane = fXMLLoader.load();
-				CatalogItemController catalogItemController = fXMLLoader.getController();
-				catalogItemController.setData(items.get(i));
-
+				Node node = fXMLLoader.load();
 				
-				grid.add(anchorPane, column++, row);
+				CatalogItemController catalogItemController = fXMLLoader.getController();
+				catalogItemController.setData(items.get(i));
+
+				grid.add(node, column++, row);
 
 				if (column == 3) {
 					column = 0;
 					row++;
 				}
 
-				GridPane.setMargin(anchorPane, new Insets(10));
+				GridPane.setMargin(node, new Insets(10));
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
 
 }
