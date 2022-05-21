36bb2558eb781ea9676490d3901eefb68787c937
WardZid <86842722+WardZid@users.noreply.github.com>
2022-05-21 14:00:24 +0300

    support

--- G18_ZL_Client/src/boundary/fxmlControllers/CustomerSupportController.java
+++ G18_ZL_Client/src/boundary/fxmlControllers/CustomerSupportController.java
@@ -32,117 +32,117 @@ import javafx.scene.layout.VBox;
 
 public class CustomerSupportController implements Initializable {
 	@FXML
-    private Label ComplaintIdL;
+	private Label ComplaintIdL;
 
-    @FXML
-    private TextField ComplaintIdT;
+	@FXML
+	private TextField ComplaintIdT;
 
-    @FXML
-    private ListView<Integer> ComplaintL;
+	@FXML
+	private ListView<Integer> ComplaintL;
 
-    @FXML
-    private Label CustomerIdL;
+	@FXML
+	private Label CustomerIdL;
 
-    @FXML
-    private TextField CustomerIdT;
+	@FXML
+	private TextField CustomerIdT;
 
-    @FXML
-    private Label StatusL;
+	@FXML
+	private Label StatusL;
 
-    @FXML
-    private Label dateL;
+	@FXML
+	private Label dateL;
 
-    @FXML
-    private TextField dateT;
+	@FXML
+	private TextField dateT;
 
-    @FXML
-    private Label descriptionL;
+	@FXML
+	private Label descriptionL;
 
-    @FXML
-    private TextArea descriptionT;
+	@FXML
+	private TextArea descriptionT;
 
-    @FXML
-    private GridPane gp;
+	@FXML
+	private GridPane gp;
 
-    @FXML
-    private HBox hb1;
+	@FXML
+	private HBox hb1;
 
-    @FXML
-    private Label refundL;
+	@FXML
+	private Label refundL;
 
-    @FXML
-    private TextField refundT;
+	@FXML
+	private TextField refundT;
 
-    @FXML
-    private Label replyL;
+	@FXML
+	private Label replyL;
 
-    @FXML
-    private TextArea replyT;
+	@FXML
+	private TextArea replyT;
 
-    @FXML
-    private Button sendReplyButton;
+	@FXML
+	private Button sendReplyButton;
 
-    @FXML
-    private StackPane stackPane;
+	@FXML
+	private StackPane stackPane;
 
-    @FXML
-    private TextField statusT;
+	@FXML
+	private TextField statusT;
 
-    @FXML
-    private VBox vb1;
+	@FXML
+	private VBox vb1;
 
-    @FXML
-    private VBox vb2;
+	@FXML
+	private VBox vb2;
 
-    @FXML
-    private VBox vb3;
-    private int selectedComplaintId;
-    private Complaint selectedComplaint;
-    public static HashMap<Integer,Complaint> ComplaintMap = new HashMap<>();
+	@FXML
+	private VBox vb3;
+	private int selectedComplaintId;
+	private Complaint selectedComplaint;
+	public static HashMap<Integer, Complaint> ComplaintMap = new HashMap<>();
 	private int isSendedReply = 0;
-    public void onSelectComplaint(ActionEvent event) {
-    	    }
-    
 
+	public void onSelectComplaint(ActionEvent event) {
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		//send to server
-		ComplaintMap.put(1, new Complaint(1,111,"OnTreatment","1/5/2022",40,"Not good",null));
-		ComplaintMap.put(2, new Complaint(2,222,"OnTreatment","2/5/2022",50,"Not good flower",null));
-		ComplaintMap.put(3, new Complaint(3,333,"OnTreatment","3/5/2022",60,"Not good workers",null));
+		// send to server
+		ComplaintMap.put(1, new Complaint(1, 111, "OnTreatment", "1/5/2022", 40, "Not good", null));
+		ComplaintMap.put(2, new Complaint(2, 222, "OnTreatment", "2/5/2022", 50, "Not good flower", null));
+		ComplaintMap.put(3, new Complaint(3, 333, "OnTreatment", "3/5/2022", 60, "Not good workers", null));
 		ComplaintL.getItems().addAll(ComplaintMap.keySet());
 		CustomerIdT.setDisable(true);
 		ComplaintIdT.setDisable(true);
 		statusT.setDisable(true);
 		dateT.setDisable(true);
 		refundT.setDisable(true);
-		descriptionT.setDisable(true);
+		descriptionT.setEditable(false);
 		sendReplyButton.setDisable(true);
 		ComplaintL.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
-		
+
 			@Override
 			public void changed(ObservableValue<? extends Integer> arg0, Integer arg1, Integer arg2) {
-				if(isSendedReply == 0) {
-				selectedComplaintId = ComplaintL.getSelectionModel().getSelectedItem();
-				selectedComplaint = ComplaintMap.get(selectedComplaintId);
-				ComplaintIdT.setText(Integer.toString(selectedComplaintId));
-				CustomerIdT.setText(Integer.toString(selectedComplaint.getIdCustomer()));
-				statusT.setText(selectedComplaint.getStatus());
-				dateT.setText(selectedComplaint.getDate());
-				refundT.setText(Double.toString(selectedComplaint.getRefund()));
-				descriptionT.setText(selectedComplaint.getComplaint());
-				//descriptionT.setDisable(true);
-				replyT.setText("");
-				sendReplyButton.setDisable(false);
+				if (isSendedReply == 0) {
+					selectedComplaintId = ComplaintL.getSelectionModel().getSelectedItem();
+					selectedComplaint = ComplaintMap.get(selectedComplaintId);
+					ComplaintIdT.setText(Integer.toString(selectedComplaintId));
+					CustomerIdT.setText(Integer.toString(selectedComplaint.getIdCustomer()));
+					statusT.setText(selectedComplaint.getStatus());
+					dateT.setText(selectedComplaint.getDate());
+					refundT.setText(Double.toString(selectedComplaint.getRefund()));
+					descriptionT.setText(selectedComplaint.getComplaint());
+					// descriptionT.setDisable(true);
+					replyT.setText("");
+					sendReplyButton.setDisable(false);
 				}
 				isSendedReply = 0;
 			}
-			
+
 		});
 	}
+
 	public void onSendReply(ActionEvent event) {
-		//sendToServer
+		// sendToServer
 		isSendedReply = 1;
 		ComplaintMap.remove(selectedComplaintId);
 		ComplaintL.getItems().clear();
