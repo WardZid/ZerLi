1e74724370334f16f7e4d93caa737a66c11deb4b
saher <saher@DESKTOP-FH43UA8>
2022-05-31 20:44:21 +0300

    update sales for catalog

--- G18_ZL_Client/src/boundary/fxmlControllers/CatalogItemController.java
+++ G18_ZL_Client/src/boundary/fxmlControllers/CatalogItemController.java
@@ -12,54 +12,66 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-public class CatalogItemController implements Initializable{
+public class CatalogItemController implements Initializable {
 
-	
 	private Item PressedItem;
 
-    @FXML
-    private Label namelabel;
+	@FXML
+	private Label namelabel;
 
-    @FXML
-    private Label pricelabel;
-    
-    @FXML
-    private VBox catalogItemVbox;
-    
-    
-    CatalogController catalogController;
+	@FXML
+	private Text priceText;
+
+	@FXML
+	private Label saleLbl;
+
+	@FXML
+	private Text salePriceText;
+
+	@FXML
+	private VBox catalogItemVbox;
+
+	CatalogController catalogController;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
-	}
-	public void setData(Item item, CatalogController catalogController) {
-		PressedItem=item;
-		 namelabel.setText(item.getName());
-		pricelabel.setText(item.getPrice()+"");
-		this.catalogController=catalogController;
-	}
-	
-	
-	public void onPressedAddItem( ) {
-		 
-		CartController.getOrderInProcess().addItemtoOrder(PressedItem);
-		catalogController.setLabelNumItemInOrderText();
-		 
-		
-	 
 
 	}
+
+	public void setData(Item item, CatalogController catalogController) {
+		 
+		PressedItem = item;
+		namelabel.setText(item.getName());
+		this.catalogController = catalogController;
+		if (item.getSale() == 0) {
+			salePriceText.setText("");
+			priceText.setText(item.getPrice() + "");
+		} else {
+			salePriceText.setText(item.getPrice()+ "");
+			item.setPriceAfterSale(item.getSale());
+			priceText.setText(item.getPrice() + "");
+			saleLbl.setVisible(true);
+
+		}
+
+	}
+
+	public void onPressedAddItem() {
+
+		CartController.getOrderInProcess().addItemtoOrder(PressedItem);
+		catalogController.setLabelNumItemInOrderText();
+
+	}
+
 	public void onItemPressed() {
-		
+
 		catalogController.getvboxViewItemDescription().setVisible(true);
 		catalogController.getcatalogvbox().setDisable(true);
 		catalogController.getnameItemLable().setText(PressedItem.getName());
 		catalogController.getdescriptionLable().setText(PressedItem.getDescription());
 	}
-	 
-	
-	
+
 }
