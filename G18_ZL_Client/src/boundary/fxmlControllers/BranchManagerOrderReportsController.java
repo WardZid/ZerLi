12577a166ec46401f9bc6388bd287003b5629d53
amer635a <72458035+amer635a@users.noreply.github.com>
2022-06-05 14:41:37 +0300

    fix ceo

--- G18_ZL_Client/src/boundary/fxmlControllers/BranchManagerOrderReportsController.java
+++ G18_ZL_Client/src/boundary/fxmlControllers/BranchManagerOrderReportsController.java
@@ -213,6 +213,7 @@ public class BranchManagerOrderReportsController implements Initializable {
 	@SuppressWarnings("unchecked")
 	public void setBranchID() {
 		ArrayList<Store> stores = (ArrayList<Store>)MainController.getMyClient().send(MessageType.GET, "store/by/id_user/"+user.getIdUser(), null);
+		if(stores.size()!=0)
 		branchID = stores.get(0).ordinal();
 	}
 	
