33f8df0e7fb80f4586b7e876ca26cf074f9e7809
WardZid <86842722+WardZid@users.noreply.github.com>
2022-06-06 19:47:16 +0300

    receipt javadoc (not finished)

--- G18_ZL_Common/src/entity/Receipt.java
+++ G18_ZL_Common/src/entity/Receipt.java
@@ -2,39 +2,63 @@ package entity;
 
 import java.io.Serializable;
 
-public class Receipt implements Serializable, Cloneable{
+public class Receipt implements Serializable, Cloneable {
 
 	private static final long serialVersionUID = 7473241707950981161L;
 	private String name;
 	private String date;
 	private double income;
-	
+
+	/**
+	 * 
+	 * @param name
+	 * @param date
+	 * @param income
+	 */
 	public Receipt(String name, String date, double income) {
 		this.name = name;
 		this.date = date;
-		this.income = income;  
+		this.income = income;
 	}
 
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the date
+	 */
 	public String getDate() {
 		return date;
 	}
 
+	/**
+	 * @param date the date to set
+	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
 
+	/**
+	 * @return the income
+	 */
 	public double getIncome() {
 		return income;
 	}
 
+	/**
+	 * @param income the income to set
+	 */
 	public void setIncome(double income) {
 		this.income = income;
 	}
@@ -43,7 +67,5 @@ public class Receipt implements Serializable, Cloneable{
 	public String toString() {
 		return "Receipt [name=" + name + ", date=" + date + ", income=" + income + "]";
 	}
-	
-	
-	
+
 }
