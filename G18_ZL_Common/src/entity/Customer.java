7ab70a0adaf59b78420d668fa715191665bdfd0c
saher <saher@DESKTOP-FH43UA8>
2022-06-07 02:06:37 +0300

    java doc

--- G18_ZL_Common/src/entity/Customer.java
+++ G18_ZL_Common/src/entity/Customer.java
@@ -1,7 +1,11 @@
 package entity;
 
 import java.io.Serializable;
-
+/**
+ * customer accounts thats belong to a user
+ * @author saher
+ *
+ */
 public class Customer implements Serializable {
 	private static final long serialVersionUID = -4182417182449829997L;
 
