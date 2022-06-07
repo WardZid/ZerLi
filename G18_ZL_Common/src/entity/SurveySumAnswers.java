0cff05e12ba5e8f1824dcb36e5237f64c2806712
saher <saher@DESKTOP-FH43UA8>
2022-06-07 03:57:15 +0300

    java doc

--- G18_ZL_Common/src/entity/SurveySumAnswers.java
+++ G18_ZL_Common/src/entity/SurveySumAnswers.java
@@ -2,16 +2,40 @@ package entity;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-
+/**
+ * this class to the answers of the survey 
+ * @author saher
+ *
+ */
 public class SurveySumAnswers  implements Serializable {
+
 	private static final long serialVersionUID = 8505837124591041487L;
+	/**
+	 * question number
+	 */
 	private int questionNumber;
+	/**
+	 * int parameter to sum the answers for every question
+	 */
 	private int sum;
-	private ArrayList<Double> avgAnswersList = new ArrayList<>();;
+	/**
+	 * ArrayList to save the average for the answers to every question on the survey 
+	 */
+	private ArrayList<Double> avgAnswersList = new ArrayList<>();
+	
+	
+	
+	
+	/**
+	 * getters and setters 
+	 */
+	
+	
 	
 	public ArrayList<Double> getAvgAnswers() {
 		return avgAnswersList;
 	}
+	
 	public void setAvgAnswers(ArrayList<Double> avgAnswersList ) {
 		this.avgAnswersList = avgAnswersList;
 	}
